696f51de0737df330e03d38f4341f80382f619b9
s_anno <amo.20325@gmail.com>
2015-01-10 15:15:52 +0900

    Initial Commit

---
+++ Promise/Promise.h
@@ -0,0 +1,19 @@
+//
+//  Promise.h
+//  Promise
+//
+//  Created by amo on 2015/01/10.
+//  Copyright (c) 2015年 amo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promise.
+FOUNDATION_EXPORT double PromiseVersionNumber;
+
+//! Project version string for Promise.
+FOUNDATION_EXPORT const unsigned char PromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promise/PublicHeader.h>
+
+
